e64d49ad3f154bbacea88d36e61edd68d10e963a
nourkhansa20 <118853035+nourkhansa20@users.noreply.github.com>
2023-06-13 13:07:30 +0300

    Update and rename InitialPortal4.js to InitialPortal5.js

--- InitialPortal5.js
+++ InitialPortal5.js
@@ -157,6 +157,10 @@ function renderCards() {
         </div>
     </div>
     <div class="cardItem">
+    <div class="infoIconContainer">
+    <img src="https://srv-k2five/Designer/Runtime/File.ashx?_path=gj%2FkK8xKydFGIaBuABcSOduRX2c%3D%5Cinformation-button.png&_filerequestdata=_4XeJqbaWuJp0syOgJHEA8of-kLRzEyrnXA94YOkjasqBnsOTQgJXJ-Ybt28RDf8-rNsOJTV6GFCRJMwfDiB-T1qyY65WRx0Csth2wTf9JOReKkiiOYspbS7vEwYNJBIywx1kBd-LFpHYtIPS0xUdrkixdScIEVBKIgcyqXW3WD2a1CNZt1TjOmkHTF0prdAe6Kyil_9PHynI0KFBGfxlSpFuMC_LFnUMkZaxgfFrVy1zuKMnYwsZLNdUnn1Fg3F02l-Z5JDdXl-ChygqFDt0QT0TpYjnxCCkjbfYOS8_pU1&_height=32&_width=32&_controltype=image&XSRFToken=4399624675727584330"
+        class='infoIcon'>
+    </div>
         <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/researchIllustration.png" class='titleImage'>
         <p class="cardTitle" id='Research'>الأبحاث</p>
         <img src='https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/employeeIconBlue.jpg' class='employeeIcon'>
